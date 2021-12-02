081d599c821ea5bf8cbcaadd10b2af9f0a7d8c23
yose216 <youness.chetoui@gmail.com>
2021-12-02 16:46:56 +0100

    logo

--- src/widgets/Menu/components/Logo.tsx
+++ src/widgets/Menu/components/Logo.tsx
@@ -24,7 +24,7 @@ const StyledLink = styled(Link)`
     }
   }
   .desktop-icon {
-    width: 160px;
+    width: 32px;
     display: none;
     ${({ theme }) => theme.mediaQueries.nav} {
       display: block;
