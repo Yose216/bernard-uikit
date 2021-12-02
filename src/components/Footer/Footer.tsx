d3107935bc45a11e86b8a85c1bab7743465ea026
yose216 <youness.chetoui@gmail.com>
2021-12-02 15:54:46 +0100

    some modif

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -38,9 +38,10 @@ const MenuItem: React.FC<FooterProps> = ({
           flexDirection={["column", null, "row"]}
           justifyContent="space-between"
         >
-        <StyledSocialLinks order={[2]} pb={["42px", null, "32px"]} mb={["0", null, "32px"]} />
 
           <Flex order={[1, null, 2]} mb={["24px", null, "0"]} justifyContent="space-between" alignItems="center">
+            <StyledSocialLinks order={[2]} pb={["42px", null, "32px"]} mb={["0", null, "32px"]} />
+
             <Box mr="20px">
               <CakePrice cakePriceUsd={cakePriceUsd} color={darkColors.textSubtle as keyof Colors} />
             </Box>
